459d4e98e1addc0e8ff30804ec3547ee3c56d4bc
benoit layer <benoit.layer@gmail.com>
2015-02-09 19:03:48 +0100

    Initial Commit

---
+++ EZClockView/EZClockView.h
@@ -0,0 +1,19 @@
+//
+//  EZClockView.h
+//  EZClockView
+//
+//  Created by Benoit Layer on 09/02/2015.
+//  Copyright (c) 2015 Benoit Layer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EZClockView.
+FOUNDATION_EXPORT double EZClockViewVersionNumber;
+
+//! Project version string for EZClockView.
+FOUNDATION_EXPORT const unsigned char EZClockViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EZClockView/PublicHeader.h>
+
+
